c59bd2c93e4beb3c533f30b9a9facd95b35d3094
clmb00 <paolo.colombo2000@gmail.com>
2022-10-27 17:37:29 +0200

    [add] print posts

--- main.js
+++ main.js
@@ -55,3 +55,47 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+posts.forEach(createPost);
+
+function createPost(elem){
+    data = formatDate(elem.created);
+    const template = `
+    <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src=${elem.author.image} alt=${elem.author.name}>                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${elem.author.name}</div>
+                    <div class="post-meta__time">${data}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${elem.content}</div>
+        <div class="post__image">
+            <img src=${elem.media} alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${elem.likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>
+    `
+    document.getElementById('container').innerHTML += template;
+}
+
+function formatDate(data){
+    const dataSplitted = data.split('-');
+    return dataSplitted[2] + '/' + dataSplitted[1] + '/' + dataSplitted[0]
+}
